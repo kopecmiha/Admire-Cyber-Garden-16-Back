a6ae37320d01dc5ea5991b3366fc4abd62da6400
Mihas <koptseff.mikhail@yandex.ru>
2022-11-25 22:26:12 +0300

    parse

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateUser, GetUserProfile, ObtainToken, UpdateUserProfile
+from .views import CreateUser, GetUserProfile, ObtainToken, UpdateUserProfile, Parse
 
 urlpatterns = [
     path('create_user/', CreateUser.as_view()),
     path('obtain_token/', ObtainToken.as_view()),
     path('get_profile/', GetUserProfile.as_view()),
     path('update_profile/', UpdateUserProfile.as_view()),
+    path('parse/', Parse.as_view()),
 ]
