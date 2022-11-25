6e06f44b92a814e5e9c198ec5d42a87981ab1f03
Mihas <koptseff.mikhail@yandex.ru>
2022-11-25 19:35:41 +0300

    fix reg, auth

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -1,20 +1,24 @@
 from rest_framework import serializers
 from .models import User
+from .utils import get_jwt_token
 
 
 class UserSerializer(serializers.ModelSerializer):
+    token = serializers.SerializerMethodField('get_jwt')
+
+    def get_jwt(self, user):
+        token = ""
+        if self.context.get("is_auth"):
+            token = get_jwt_token(user)
+        return token
+
     class Meta(object):
         model = User
-        fields = "first_name", "last_name", "patronymic", "email", 'password', "uuid",
-        extra_kwargs = {'uuid': {'read_only': True}}
+        fields = "first_name", "last_name", "patronymic", "email", "uuid", "token",
+        extra_kwargs = {'uuid': {'read_only': True}, "token": {'read_only': True}}
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = User
-        fields = "email", 'password', "username"
-
-
-class UserAuthSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-    token = serializers.CharField()
+        fields = "email", 'password', "username", "first_name", "last_name", "patronymic",
