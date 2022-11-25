4059ad9a4a8b03b7184972eb3947624511605741
Mihas <koptseff.mikhail@yandex.ru>
2022-11-26 01:52:53 +0300

    filter full

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('create_user/', CreateUser.as_view()),
     path('obtain_token/', ObtainToken.as_view()),
     path('get_profile/', GetUserProfile.as_view()),
-    path('user_profile/<str:id>', UserProfile.as_view()),
+    path('user_profile/<str:id>/', UserProfile.as_view()),
     path('update_profile/', UpdateUserProfile.as_view()),
     path('list_of_users/', GetListOfUsers.as_view()),
     path('list_of_users_filter/', GetListOfUsersFilter.as_view(),),
