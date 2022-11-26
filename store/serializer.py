13a1d73d25a6a3c69ca250ade62e2d6a9123936c
Mihas <koptseff.mikhail@yandex.ru>
2022-11-26 10:19:20 +0300

    add store module

---
+++ store/serializer.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Products
+from department.models import Department
+
+
+class ProductsSerializer(serializers.ModelSerializer):
+
+    class Meta(object):
+        model = Products
+        fields = ["id", "title", "icons", "price", "in_stock",]
+        extra_kwargs = {"id": {'read_only': True}, "icons": {'read_only': True}}
