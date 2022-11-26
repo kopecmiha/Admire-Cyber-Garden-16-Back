0cdf74a31723e4f6b92d59c3b3fc04327739b2c6
grakky <akimgrebenkin21@gmail.com>
2022-11-26 11:08:25 +0300

    collection_ready

--- collection/urls.py
+++ collection/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import SpawnPlayCard, PlayCardsView, UserPlayCardsView
+from .views import SpawnPlayCard, PlayCardsView, UserPlayCardsView, DepartmentPlayCardView, UserDepartmentPlayCardView
 
 urlpatterns = [
     path('spawn_playcard/', SpawnPlayCard.as_view()),
     path('play_cards/', PlayCardsView.as_view()),
     path('user_play_cards/<str:user_id>/', UserPlayCardsView.as_view()),
-   # path('get_department/<int:department_id>', GetDepartment.as_view()),
-   # path('add_to_department/', AddToDepartment.as_view()),
-
+    path('play_card_departments/', DepartmentPlayCardView.as_view()),
+    path('user_play_card_departments/<str:user_id>/', UserDepartmentPlayCardView.as_view()),
 ]
