24abdc04c8f16e9b7306fceabbfcac4005d64712
grakky <akimgrebenkin21@gmail.com>
2022-11-26 22:04:46 +0300

    card_ref

--- collection/models.py
+++ collection/models.py
@@ -10,7 +10,8 @@ class PlayCard(models.Model):
     person = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=False, null=False, related_name="collection_person")
 
 
-'''class CardTrade(models.Model):
+class CardTrade(models.Model):
     user1 = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=False, null=False, related_name="trade_user1")
     user2 = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=False, null=False, related_name="trade_user2")
-    offered_cards = ArrayField()'''
+    user1_cards = ArrayField(models.IntegerField(), default=list, null=True, blank=True)
+    user2_cards = ArrayField(models.IntegerField(), default=list, null=True, blank=True)
