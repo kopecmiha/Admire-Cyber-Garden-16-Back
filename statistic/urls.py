6f2a76c39036381f4e327a79aa28e4093c0d7aa2
Mihas <koptseff.mikhail@yandex.ru>
2022-11-26 13:36:27 +0300

    trading

--- statistic/urls.py
+++ statistic/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import ReplenishmentGameSession, GetUserPoints
+from .views import ReplenishmentGameSession, GetUserBalance
 
 urlpatterns = [
     path('add_game_session/', ReplenishmentGameSession.as_view()),
-    path('get_user_points/', GetUserPoints.as_view()),
+    path('get_user_balance/', GetUserBalance.as_view()),
 ]
